6d5ae9d79ba19de4d77b28f6221146074ee4f436
Aidan Marshall <aidanpm.marshall@gmail.com>
2024-01-19 07:56:23 +0000

    Setup C# basic calculator

---
+++ src/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static int Add(int x, int y)
+    {
+      return x + y;
+    }
+
+    static int Subtract(int x, int y) 
+    {
+        return x - y;
+    }
+
+    static int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+
+    static int Divide(int x, int y)
+    {
+        return x / y;
+    }
+
+    static void Main(string[] args)
+    {
+        while (true) {
+        Console.WriteLine("Enter a basic calculation:");
+        string userInput = Console.ReadLine();
+
+        string[] formattedInput = userInput.Split();
+        
+        int firstNum = int.Parse(formattedInput[0]);
+        int secondNum = int.Parse(formattedInput[2]);
+        string function = formattedInput[1];
+
+        if (userInput == "q")
+        {
+            break;
+        } 
+        // else-if (function == "+") {
+        //     int result = Add(firstNum, secondNum);
+        //     Console.WriteLine($"{userInput} equals {result}");
+        // } else {
+        //     Console.WriteLine("Don't know buddy");
+        // }
+        }
+
+    }  
+  }
+}
