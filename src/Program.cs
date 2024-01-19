2635474c835b8f21144bf3d47ae06d9aa3578e95
Aidan Marshall <aidanpm.marshall@gmail.com>
2024-01-19 10:26:53 +0000

    deleted cache files, fixing validation

--- src/Program.cs
+++ src/Program.cs
@@ -28,11 +28,32 @@ namespace Calculator
         Console.WriteLine($"{userInput} equals {result}");
     }
 
+    static bool NotValid(string? userInput) {
+        userInput = userInput.ToLower();
+        if (userInput == "q") {
+            return false;
+        }
+
+        return string.IsNullOrEmpty(userInput) || userInput.Any(char.IsLetter)
+                || userInput.Length < 5 || !char.IsDigit(userInput, 0) || !char.IsDigit(userInput, 4);
+    }
+
+    static string GetUserInput() {
+        Console.WriteLine("Enter a basic calculation. User 'help' for more information.");
+        string? userInput = Console.ReadLine();
+
+        while (NotValid(userInput))  {
+            Console.WriteLine("Invalid input. Please enter a calculation in the following format: 1 + 1 - use the help command to see available functions.");
+            userInput = Console.ReadLine();
+        }
+        return userInput;
+    }
+
     static void Main(string[] args)
     {
         while (true) {
         Console.WriteLine("Enter a basic calculation:");
-        string userInput = Console.ReadLine();
+        string userInput = GetUserInput();
 
         string[] formattedInput = userInput.Split();
         
@@ -57,15 +78,15 @@ namespace Calculator
             int result = Divide(firstNum, secondNum);
             DisplayResult(userInput, result.ToString());
         }
-        else if (function == "x") {
+        else if (function == "*") {
             int result = Multiply(firstNum, secondNum);
             DisplayResult(userInput, result.ToString());
         }
         else {
-            Console.WriteLine("Don't know buddy");
+            Console.WriteLine("An error has occured.");
+            break;
         }
         }
-
     }  
   }
 }
