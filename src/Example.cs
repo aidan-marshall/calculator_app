dd16d04ce784f4cc688b82b289d25edeea6fed67
Aidan Marshall <aidanpm.marshall@gmail.com>
2024-01-19 14:30:41 +0000

    starting point

--- src/Example.cs
+++ src/Example.cs
@@ -2,22 +2,4 @@
 
 namespace CalculatorApp.Example
 {
-    public class Calculator
-    {
-        public int Add(int num1, int num2) {
-          return num1 + num2;
-        }
-
-        public int Subtract(int num1, int num2) {
-          return num1 - num2;
-        }
-
-        public int Multiply(int num1, int num2) {
-          return num1 * num2;
-        }
-
-        public int Divide(int num1, int num2) {
-          return num1 / num2;
-        }
-    }
 }
--- tests/CalculatorTests.cs
+++ tests/CalculatorTests.cs
@@ -9,51 +9,6 @@ namespace CalculatorApp.UnitTests.Example
 {
     public class CalculatorTests
     {
-        private readonly Calculator _newcalculator;
         
-        public CalculatorTests()
-        {
-            _newcalculator = new Calculator();
-        }
-        
-        // First Test for NewMethod
-        [Fact]
-        public void TwoPlusTwoEqualsFour()
-        {
-            int result = _newcalculator.Add(2, 2);
-            int expectedResult = 4;
-
-
-            Assert.Equal(expectedResult, result);
-        }
-
-        [Fact]
-        public void TwentyMinusFiveEqulasFifteen() 
-        {
-            int result = _newcalculator.Subtract(10, 5);
-            int expectedResult = 5;
-
-            Assert.Equal(expectedResult, result);
-        }
-
-        [Fact]
-        public void FiveMultipliedByFiveEqualsTwentyFive() {
-
-            int result = _newcalculator.Multiply(5, 5);
-            int expectedResult = 25;
-
-            Assert.Equal(expectedResult, result);
-    
-        }
-
-        [Fact]
-        public void TwentyDividedByTwoEqualsTen() {
-
-            int result = _newcalculator.Divide(20, 2);
-            int expectedResult = 10;
-
-            Assert.Equal(expectedResult, result);
-        }
-
     }
 }
